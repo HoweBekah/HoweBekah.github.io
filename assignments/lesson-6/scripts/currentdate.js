bb99b66573b17a491371c6c46e5ebcee9c612e6d
HoweBekah <how18019@byui.edu>
2019-02-15 20:38:28 -0700

    datejs work

---
+++ assignments/lesson-6/scripts/currentdate.js
@@ -0,0 +1,42 @@
+var year = getFullYear();
+var monthnum = getMonth();
+switch (monthnum) {
+    case 0:
+        var month = "January";
+        break;
+    case 1:
+        var month = "February";
+        break;
+    case 2:
+        var month = "March";
+        break;
+    case 3:
+        var month = "April";
+        break;
+    case 4:
+        var month = "May";
+        break;
+    case 5:
+        var month = "June";
+        break;
+    case 6:
+        var month = "July";
+        break;
+    case 7:
+        var month = "August";
+        break;
+    case 8:
+        var month = "September";
+        break;
+    case 9:
+        var month = "October";
+        break;
+    case 10:
+        var month = "November";
+        break;
+    case 11:
+        var month = "December";
+        break;
+}
+var datenum = getDate();
+var daynum = getDay();

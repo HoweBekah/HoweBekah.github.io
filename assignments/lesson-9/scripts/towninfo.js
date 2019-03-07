6df21becc727b8c11c392b0a484ca5878adc4d5b
HoweBekah <how18019@byui.edu>
2019-03-07 16:38:38 -0700

    css fixes

--- assignments/lesson-9/scripts/towninfo.js
+++ assignments/lesson-9/scripts/towninfo.js
@@ -1,42 +1,43 @@
-var section = document.querySelector('section');
+var section = document.querySelector("section");
 
 var pics = [
-    "https://howebekah.github.io/assignments/lesson-9/images/preston.jpg",
-    "https://howebekah.github.io/assignments/lesson-9/images/sodasprings.jpg",
-    "https://howebekah.github.io/assignments/lesson-9/images/fishhaven.jpg"
+  "https://howebekah.github.io/assignments/lesson-9/images/preston.jpg",
+  "https://howebekah.github.io/assignments/lesson-9/images/sodasprings.jpg",
+  "https://howebekah.github.io/assignments/lesson-9/images/fishhaven.jpg"
 ];
 
-var requestURL = 'https://howebekah.github.io/assignments/lesson-9/json/towndata.json';
+var requestURL =
+  "https://howebekah.github.io/assignments/lesson-9/json/towndata.json";
 var request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'text';
+request.open("GET", requestURL);
+request.responseType = "text";
 request.send();
 
-request.onload = function () {
-    var townText = request.response;
-    var towns = JSON.parse(townText);
-    populateTown(towns, pics);
-}
+request.onload = function() {
+  var townText = request.response;
+  var towns = JSON.parse(townText);
+  populateTown(towns, pics);
+};
 
 function populateTown(jObj, jPics) {
-    var towns = jObj['towns'];
+  var towns = jObj["towns"];
 
-    for (var i = 0; i < towns.length; i++) {
-        var articleC = document.createElement('article');
-        var h3C = document.createElement('h3');
-        var para1C = document.createElement('p');
-        var para2C = document.createElement('p');
-        var para3C = document.createElement('p');
-        var para4C = document.createElement('p');
-        //var listC = document.createElement('ul');
+  for (var i = 0; i < towns.length; i++) {
+    var articleC = document.createElement("article");
+    var h3C = document.createElement("h3");
+    var para1C = document.createElement("p");
+    var para2C = document.createElement("p");
+    var para3C = document.createElement("p");
+    var para4C = document.createElement("p");
+    //var listC = document.createElement('ul');
 
-        h3C.textContent = towns[i].name;
-        para1C.textContent = '"' + towns[i].motto + '"';
-        para2C.textContent = 'Year Founded: ' + towns[i].yearFounded;
-        para3C.textContent = 'Current Population: ' + towns[i].currentPopulation;
-        para4C.textContent = 'Average Rainfall: ' + towns[i].averageRainfall;
-        para1C.style.fontWeight = 'bolder';
-        /* var powers = heroes[i].powers;
+    h3C.textContent = towns[i].name;
+    para1C.textContent = '"' + towns[i].motto + '"';
+    para2C.textContent = "Year Founded: " + towns[i].yearFounded;
+    para3C.textContent = "Current Population: " + towns[i].currentPopulation;
+    para4C.textContent = "Average Rainfall: " + towns[i].averageRainfall;
+    para1C.style.fontWeight = "bolder";
+    /* var powers = heroes[i].powers;
         for (var j = 0; j < powers.length; j++) {
             var listItem = document.createElement('li');
             listItem.textContent = powers[j];
@@ -44,22 +45,22 @@ function populateTown(jObj, jPics) {
         }
 */
 
-        articleC.appendChild(h3C);
-        articleC.appendChild(para1C);
-        articleC.appendChild(para2C);
-        articleC.appendChild(para3C);
-        articleC.appendChild(para4C);
-        //articleC.appendChild(listC);
+    articleC.appendChild(h3C);
+    articleC.appendChild(para1C);
+    articleC.appendChild(para2C);
+    articleC.appendChild(para3C);
+    articleC.appendChild(para4C);
+    //articleC.appendChild(listC);
 
-        for (var j = 0; j < jPics.length; j++) {
-            var pic = document.createElement('img');
-            pic.src = jPics[j];
-            articleC.appendChild(pic);
-
-        }
-
-        section.appendChild(articleC);
+    for (var j = i; j < jPics.length; ) {
+      var pic = document.createElement("img");
+      pic.src = jPics[j];
+      articleC.appendChild(pic);
+      break;
     }
+
+    section.appendChild(articleC);
+  }
 }
 
 /*function populatePic(jPics) {

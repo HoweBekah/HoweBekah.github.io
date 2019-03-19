11894ac2a49861d08953e33c9580b3a6d2d62da6
HoweBekah <how18019@byui.edu>
2019-03-19 14:51:00 -0700

    maybe fixed

--- assignments/lesson-10/scripts/eventpop.js
+++ assignments/lesson-10/scripts/eventpop.js
@@ -13,7 +13,7 @@ request.onload = function () {
     populateEvents(towns);
 }
 
-function populateEvents(jObj) {
+/*function populateEvents(jObj) {
     var townList = jObj["towns"];
     var listC = document.createElement('ul');
     var events = townList[0].events;
@@ -23,4 +23,31 @@ function populateEvents(jObj) {
         listC.appendChild(listItem);
     }
     eventid.appendChild(listC);
+}*/
+function populateEvents(jObj) {
+    var townList = jObj['towns']; // Easy access to the towns item in the JSON.
+    var listC = document.createElement('ul');
+    // First find town == Preston at townIndex. Second loop through events at townList[townIndex].events. Third add the text to some DOM element.
+    for (var townI = 0; townI < townList.length; townI++) { // Find town == aTownName by looping through all the towns until we find it.
+        if (document.URL.indexOf("preston") > -1) { // If we find a matching town...
+            for (var eventI = 0; eventI < townList[0].events.length; eventI++) { // Loop through all events for aTownName and show them on the DOM.
+                var listItem = document.createElement('li');
+                listItem.textContent = townList[0].events[eventI];
+                listC.appendChild(listItem); // Append the P to a Div pre-created to display preston events.
+            }
+        } else if (townList[townI].name == 'Soda Springs') { // If we find a matching town... 
+            for (var eventI = 0; eventI < townList[townI].events.length; eventI++) { // Loop through all events for aTownName and show them on the DOM.
+                var listItem = document.createElement('li');
+                listItem.textContent = townList[townI].events[eventI];
+                listC.appendChild(listItem); // Append the P to a Div pre-created to display preston events.
+            }
+        } else { // If we find a matching town... 
+            for (var eventI = 0; eventI < townList[townI].events.length; eventI++) { // Loop through all events for aTownName and show them on the DOM.
+                var listItem = document.createElement('li');
+                listItem.textContent = townList[townI].events[eventI];
+                listC.appendChild(listItem); // Append the P to a Div pre-created to display preston events.
+            }
+        }
+        eventid.appendChild(listC);
+    }
 }
